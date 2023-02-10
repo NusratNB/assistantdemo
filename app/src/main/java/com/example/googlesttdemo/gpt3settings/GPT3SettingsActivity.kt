3a7458d015d5137880eb3e2bb348761782244193
nb2904 <44426608+NusratNB@users.noreply.github.com>
2023-02-10 19:48:43 +0900

    Update

--- app/src/main/java/com/example/googlesttdemo/gpt3settings/GPT3SettingsActivity.kt
+++ app/src/main/java/com/example/googlesttdemo/gpt3settings/GPT3SettingsActivity.kt
@@ -2,9 +2,8 @@ package com.example.googlesttdemo.gpt3settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.SeekBar
-import android.widget.Spinner
+import android.util.Log
+import android.widget.*
 import com.example.googlesttdemo.R
 import com.google.android.material.textfield.TextInputEditText
 
@@ -13,13 +12,24 @@ class GPT3SettingsActivity : AppCompatActivity() {
     private lateinit var spModel: Spinner
     private lateinit var etMaxTokens: TextInputEditText
     private lateinit var skTemperature: SeekBar
-    private lateinit var etTopP: TextInputEditText
+    private lateinit var skTopP: SeekBar
     private lateinit var etN: TextInputEditText
     private lateinit var spStream: Spinner
     private lateinit var spLogProbs: Spinner
     private lateinit var skPresencePenalty: SeekBar
     private lateinit var skFrequencyPenalty: SeekBar
     private lateinit var btnSaveSettings: Button
+    private lateinit var txtModel: TextView
+    private lateinit var txtMaxTokens: TextView
+    private lateinit var txtTemperature: TextView
+    private lateinit var txtTopP: TextView
+    private lateinit var txtN: TextView
+    private lateinit var txtStream: TextView
+    private lateinit var txtLogProbs: TextView
+    private lateinit var txtPresencePenalty: TextView
+    private lateinit var txtFrequencyPenalty: TextView
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +39,122 @@ class GPT3SettingsActivity : AppCompatActivity() {
         spModel = findViewById(R.id.spModel)
         etMaxTokens = findViewById(R.id.etMaxTokens)
         skTemperature = findViewById(R.id.skTemperature)
-        etTopP = findViewById(R.id.etTopP)
+        skTopP = findViewById(R.id.skTopP)
         etN = findViewById(R.id.etN)
         spStream = findViewById(R.id.spStream)
         spLogProbs = findViewById(R.id.spLogProbs)
         skPresencePenalty = findViewById(R.id.skPresencePenalty)
         skFrequencyPenalty = findViewById(R.id.skFrequencyPenalty)
-        btnSaveSettings = findViewById(R.id.btnSettings)
+        btnSaveSettings = findViewById(R.id.btnSaveSettings)
+        txtModel = findViewById(R.id.txtModel)
+        txtMaxTokens = findViewById(R.id.txtMaxTokens)
+        txtTemperature = findViewById(R.id.txtTemperature)
+        txtTopP = findViewById(R.id.txtTopP)
+        txtN = findViewById(R.id.txtN)
+        txtStream = findViewById(R.id.txtStream)
+        txtLogProbs = findViewById(R.id.txtLogProbs)
+        txtPresencePenalty = findViewById(R.id.txtPresencePenalty)
+        txtFrequencyPenalty = findViewById(R.id.txtFrequencyPenalty)
+
+
+
+
+        val gpt3ModelOptions = arrayOf("text-davinci-003", "text-curie-001", "text-babbage-001", "text-ada-001")
+        val streamOptions = arrayOf(false, true)
+        val logprobsOptions = arrayOf("null", 1, 2, 3, 4, 5)
+        val adapterGPT3Models = ArrayAdapter(this, android.R.layout.simple_spinner_item, gpt3ModelOptions)
+        val adapterStream = ArrayAdapter(this, android.R.layout.simple_spinner_item, streamOptions)
+        val adapterLogprobs = ArrayAdapter(this, android.R.layout.simple_spinner_item, logprobsOptions)
+
+        spModel.adapter = adapterGPT3Models
+        spStream.adapter = adapterStream
+        spLogProbs.adapter = adapterLogprobs
+
+        skTemperature.max = 100
+        skTemperature.progress = 0
+
+        var progressValueTemperature = 0f
+
+        skTemperature.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                progressValueTemperature = progress.toFloat() / 100f
+                txtTemperature.text = "temperature: ${"%.2f".format(progressValueTemperature)}"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Handle the start of a touch gesture here
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Handle the end of a touch gesture here
+            }
+        })
+
+        skTopP.max = 100
+        skTopP.progress = 0
+
+        var progressValueTopP = 0f
+
+        skTopP.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                progressValueTopP = progress.toFloat() / 100f
+                txtTopP.text = "top_p: ${"%.2f".format(progressValueTopP)}"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Handle the start of a touch gesture here
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Handle the end of a touch gesture here
+            }
+        })
+
+        skPresencePenalty.max = 400
+        skPresencePenalty.progress = 0
+
+        var skPresencePenaltyValue = 0f
+
+        skPresencePenalty.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                skPresencePenaltyValue = (progress.toFloat() - 200f) / 100f
+                txtPresencePenalty.text = "presence_penalty: ${"%.2f".format(skPresencePenaltyValue)}"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+        skFrequencyPenalty.max = 400
+        skFrequencyPenalty.progress = 0
+
+        var skFrequencyPenaltyValue = 0f
+
+        skFrequencyPenalty.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                skFrequencyPenaltyValue = (progress.toFloat() - 200f) / 100f
+                txtFrequencyPenalty.text = "frequency_penalty: ${"%.2f".format(skFrequencyPenaltyValue)}"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+
+
+        btnSaveSettings.setOnClickListener {
+        }
     }
+
+
+
+
 }
