9026e3c180ba1d5f86718d6e5ca64cb383fc4dc5
Nusrat Ismoilov <nusrat.bbismoilov@gmail.com>
2023-02-13 00:10:45 +0900

    Finalizing GPT3 Settings page

--- app/src/main/java/com/example/googlesttdemo/gpt3settings/GPT3SettingsActivity.kt
+++ app/src/main/java/com/example/googlesttdemo/gpt3settings/GPT3SettingsActivity.kt
@@ -59,10 +59,29 @@ class GPT3SettingsActivity : AppCompatActivity() {
         txtPresencePenalty = findViewById(R.id.txtPresencePenalty)
         txtFrequencyPenalty = findViewById(R.id.txtFrequencyPenalty)
 
+        val model = mPreferences.getString("model", "text-curie-001")
+        val max_tokens = mPreferences.getString("max_tokens", "1000")
+        val temperature = mPreferences.getString("temperature", "0")
+        val top_p = mPreferences.getString("top_p", "1")
+        val n = mPreferences.getString("n", "1")
+        val stream = mPreferences.getString("stream", "false")
+        val logprobs = mPreferences.getString("logprobs", "null")
+        val frequency_penalty = mPreferences.getString("frequency_penalty", "0")
+        val presence_penalty = mPreferences.getString("presence_penalty", "0.6")
 
+        /* Default-settings
+        model = 'text-curie-001
+        max_tokens = 1000
+        temperature = 0
+        top_p = 1
+        n = 1
+        stream = false
+        logprobs = null
+        frequency_penalty=0
+        presence_penalty=0.6
+        */
 
-
-        val gpt3ModelOptions = arrayOf("text-davinci-003", "text-curie-001", "text-babbage-001", "text-ada-001")
+        val gpt3ModelOptions = arrayOf("text-curie-001", "text-davinci-003",  "text-babbage-001", "text-ada-001")
         val streamOptions = arrayOf(false, true)
         val logprobsOptions = arrayOf("null", 1, 2, 3, 4, 5)
         val adapterGPT3Models = ArrayAdapter(this, android.R.layout.simple_spinner_item, gpt3ModelOptions)
@@ -75,6 +94,8 @@ class GPT3SettingsActivity : AppCompatActivity() {
 
         skTemperature.max = 100
         skTemperature.progress = 0
+        txtTemperature.text = "temperature: $temperature"
+
 
         var progressValueTemperature = 0f
 
@@ -97,6 +118,7 @@ class GPT3SettingsActivity : AppCompatActivity() {
         skTopP.progress = 0
 
         var progressValueTopP = 0f
+        txtTopP.text = "top_p: $top_p"
 
         skTopP.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -117,6 +139,7 @@ class GPT3SettingsActivity : AppCompatActivity() {
         skPresencePenalty.progress = 0
 
         var skPresencePenaltyValue = 0f
+        txtPresencePenalty.text = "presence_penalty: $presence_penalty"
 
         skPresencePenalty.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
@@ -137,6 +160,8 @@ class GPT3SettingsActivity : AppCompatActivity() {
 
         var skFrequencyPenaltyValue = 0f
 
+        txtFrequencyPenalty.text = "frequency_penalty: $frequency_penalty"
+
         skFrequencyPenalty.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
                 skFrequencyPenaltyValue = (progress.toFloat() - 200f) / 100f
@@ -149,6 +174,12 @@ class GPT3SettingsActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(p0: SeekBar?) {
             }
         })
+
+        txtModel.text = "model: $model"
+        txtMaxTokens.text = "max_tokens: $max_tokens"
+        txtN.text = "n: $n"
+        txtStream.text = "stream: $stream"
+        txtLogProbs.text = "logprobs: $logprobs"
 
         btnSaveSettings.setOnClickListener {
             val spModelSelectedItem = spModel.selectedItem.toString()
@@ -167,10 +198,19 @@ class GPT3SettingsActivity : AppCompatActivity() {
             txtPresencePenalty.text = "presence_penalty: $skPresencePenaltyValue"
             txtFrequencyPenalty.text = "frequency_penalty: $skFrequencyPenaltyValue"
 
+
             mPreferences.edit()
-                .putString("gpt3_model", spModelSelectedItem)
-                .putString("gpt3_max_tokens", etMaxTokensInpText)
+                .putString("model", spModelSelectedItem)
+                .putString("max_tokens", etMaxTokensInpText)
+                .putString("temperature", progressValueTemperature.toString())
+                .putString("top_p", progressValueTopP.toString())
+                .putString("n", etNInpText)
+                .putString("stream", spStreamSelectedItem)
+                .putString("logprobs", spLogProbsSelectedItem)
+                .putString("frequency_penalty", skFrequencyPenaltyValue.toString())
+                .putString("presence_penalty", skPresencePenaltyValue.toString())
                 .apply()
+
             Log.d("gpt3Set spModelSelectedItem", spModelSelectedItem)
             Log.d("gpt3Set spStreamSelectedItem", spStreamSelectedItem)
             Log.d("gpt3Set spLogProbsSelectedItem", spLogProbsSelectedItem)
@@ -184,13 +224,7 @@ class GPT3SettingsActivity : AppCompatActivity() {
 
         }
 
-        val gpt3Model = mPreferences.getString("gpt3_model", null)
-        if (gpt3Model != null) {
-            spModel.setSelection(gpt3ModelOptions.indexOf(gpt3Model)) // saqlangan modelni positionini aniqlab uni spinnerga set qilamiz
-        }
 
-        val gpt3MaxTokens = mPreferences.getString("gpt3_max_tokens", "")
-        etMaxTokens.setText(gpt3MaxTokens)
     }
 
 }
